ee48d362bd32a15a3639c0bdb6cc4157ae2deed7
Mark Hayes <mark@zurb.com>
2012-08-02 16:22:14 -0700

    raptorize needs to be loaded in footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -104,6 +104,7 @@
   <!-- Included JS Files (Compressed) -->
   <script src="javascripts/jquery.min.js"></script>
   <script src="javascripts/foundation.min.js"></script>
+  <script src="javascripts/jquery.raptorize.js"></script>
   
   <!-- Application Javascript, safe to override -->
   <script src="javascripts/app.js"></script>
