c65f1c5d40ba79b948bb533d842771b6136485d3
Arthur <arthur@zurb.com>
2012-08-02 15:28:46 -0700

    Sponsor page

---
+++ sponsor.php
@@ -0,0 +1,9 @@
+<?
+	$page_title = "Volunteer or Host" ;
+	$page_template = "main";	
+?>
+<?php include("includes/_header.php"); ?>
+
+<!-- HOMEPAGE CONTENT -->
+
+<?php include("includes/_footer.php");  ?>
