0beb95b9036aa0a4901e9c2a93d1f68784d676e2
Meredith Dodge <meredith.dodge@gmail.com>
2012-08-02 17:14:04 -0700

    Checking in volunteer page so far...

---
+++ volunteer.php
@@ -0,0 +1,79 @@
+<?
+	$page_title = "Volunteer" ;
+	$page_template = "main";	
+?>
+<?php include("includes/_header.php"); ?>
+
+<!-- VOLUNTEER PAGE CONTENT -->
+<div class="row">
+  <div class="twelve column"> <img src="http://placehold.it/1000x250">
+    <h1>Volunteer</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="three column">
+    <ul class="side-nav">
+      <li><a href="#joinus">Join Us</a></li>
+      <li><a href="#angelelf">Angel Elf</a></li>
+      <li><a href="#warehouse">The Warehouse</a></li>
+      <li><a href="#events">Events</a>
+      <li><a href="#celebrating">Celebrating Volunteers</a></li>
+    </ul>
+  </div>
+  <div class="nine column">
+    <section id="joinus">
+      <h2>Join Us</h2>
+      <div class="panel">
+        <h5>When can you volunteer?</h5>
+        <label for="day">Day</label>
+        <select>
+          <option value="Warehouse Set UP">Wednesday, Jul 25, 2012</option>
+          <option value="Warehouse Set UP">Tuesday, Jul 31, 2012</option>
+          <option value="Unloading backpacks/Sorting by grade level">Wednesday, Aug 1, 2012</option>
+          <option value="QA on Backpack content">Thursday, Aug 2, 2012</option>
+          <option value="QA/Backpack assembly">Friday, Aug 3, 2012</option>
+          <option value="QA/Backpack Assembly">Saturday, Aug 4, 2012</option>
+          <option value="QA/Backpack Assembly">Monday, Aug 6, 2012</option>
+          <option value="QA/Backpack Assembly">Tuesday, Aug 7, 2012</option>
+          <option value="QA/Backpack Assembly">Wednesday, Aug 8, 2012</option>
+          <option value="QA/Backpack Assembly">Thursday, Aug 9, 2012</option>
+          <option value="QA/Putting Backpacks in Rows for Schools">Friday, Aug 10, 2012</option>
+          <option value="Rowing/Loading Backpack for Pick Up">Monday, Aug 13, 2012</option>
+          <option value="Rowing/Loading Backpacks">Tuesday, Aug 14, 2012</option>
+          <option value="Rowing/Loading Backpacks">Wednesday, Aug 15, 2012</option>
+          <option value="Rowing/loading Backpacks">Thursday, Aug 16, 2012</option>
+          <option value="Loading Backpacks">Friday, Aug 17, 2012</option>
+          <option value="Packing/Clean Up">Monday, Aug 20, 2012</option>
+          <option value="Packing/Clean Up">Tuesday, Aug 21, 2012</option>
+          <option value="Packing/Clean Up">Wednesday, Aug 22, 2012</option>
+          <option value="Packing/Clean Up">Thursday, Aug 23, 2012</option>
+          <option value="Packing/Clean Up">Friday, Aug 24, 2012</option>
+        </select>
+        <a href="#" class="button" data-reveal-id="registerModal">Click Me For A Modal</a> </div>
+    </section>
+    <section id="angelelf">
+      <h2>Angel Elves</h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere gravida magna in tristique. Nam pretium augue sit amet tortor varius vitae venenatis magna semper. Proin quis erat et massa consectetur feugiat. Morbi sollicitudin mattis ligula, non vehicula est rutrum id. Ut cursus lorem a leo aliquet auctor. Suspendisse elit felis, pellentesque sed gravida rutrum, ultrices at erat. Aliquam vel ultricies urna. In interdum luctus magna ac tincidunt. Nullam tortor ligula, venenatis ut aliquet in, lobortis eget nisl. Ut quis elit mauris, et congue ligula. Mauris vitae tellus non mauris rutrum ultricies a pulvinar nisi. Morbi facilisis dignissim turpis, non aliquet nibh scelerisque ut. Donec quis lacus lectus, in venenatis tellus.</p>
+      <p class="text-right readmore">Read More</p>
+      <div class="readmorecontent">
+      <p> Quisque volutpat lobortis facilisis. Vivamus dictum placerat faucibus. Mauris lectus leo, tempor ut lacinia nec, consequat vitae diam. Sed eget eleifend nibh. Aliquam eget tortor at diam vehicula cursus. Nam ultrices tellus id risus pharetra facilisis. Nulla quam felis, imperdiet in scelerisque quis, pharetra sed mauris. Vivamus in justo dapibus ipsum condimentum ornare vel id metus. Curabitur rutrum eleifend ligula quis sodales. Nullam rhoncus porta magna, at suscipit felis consectetur mattis. </p>
+      </div>
+    </section>
+    <section id="warehouse">
+      <h2>The Warehouse</h2>
+    </section>
+    <section id="events">
+      <h2>Events</h2>
+    </section>
+    <section id="celebrating">
+      <h2>Celebrating Volunteers</h2>
+    </section>
+  </div>
+</div>
+<?php include("includes/_footer.php");  ?>
+<div id="registerModal" class="reveal-modal">
+  <h2>Awesome. I have it.</h2>
+  <p class="lead">Your couch. I it's mine.</p>
+  <p>Im a cool paragraph that lives inside of an even cooler modal. Wins</p>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
