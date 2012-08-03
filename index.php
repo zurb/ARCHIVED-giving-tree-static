d7bba006c58c89301a488a27780fccf019ce017a
Mark Hayes <mark@zurb.com>
2012-08-02 20:43:16 -0700

    using spin.js for orbit loading

--- index.php
+++ index.php
@@ -117,30 +117,12 @@
 </div>
 
 <?php content_for('footer', function() { ?>
-  <script src="http://fgnass.github.com/spin.js/dist/spin.min.js"></script>
   <script type="text/javascript"> 
-     $(window).load(function() {
-         $('#featured').orbit({ fluid: '16x6' });
-         var opts = {
-           lines: 13, // The number of lines to draw
-           length: 7, // The length of each line
-           width: 4, // The line thickness
-           radius: 10, // The radius of the inner circle
-           rotate: 0, // The rotation offset
-           color: '#000', // #rgb or #rrggbb
-           speed: 1, // Rounds per second
-           trail: 60, // Afterglow percentage
-           shadow: false, // Whether to render a shadow
-           hwaccel: false, // Whether to use hardware acceleration
-           className: 'spinner', // The CSS class to assign to the spinner
-           zIndex: 2e9, // The z-index (defaults to 2000000000)
-           top: 'auto', // Top position relative to parent in px
-           left: 'auto' // Left position relative to parent in px
-         };
-         debugger
-         var target = document.getElementById('featured');
-         var spinner = new Spinner(opts).spin(target);
-     });
+    $(".row.featured").spin({lines:9, width:4})
+   $(window).load(function() {
+       $('#featured').orbit({ fluid: '16x6' });
+       $(".row.featured").spin(false);
+   });
   </script>
 <?php })?>
 
