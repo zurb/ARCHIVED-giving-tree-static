0e44250b768fe470c70d882a31a2681e6821ea0e
Matt <matt@zurb.com>
2012-08-03 05:14:51 -0700

    Use old donate button

--- donate.php
+++ donate.php
@@ -25,14 +25,14 @@
     <div class="donate panel">
       <h3>Give Money</h3>
       <p>Give a one time or monthly monetary donation!</p>
-      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <!-- <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
       <input type="hidden" name="cmd" value="_s-xclick">
       <input type="hidden" name="hosted_button_id" value="E8CTCYY6FGSU4">
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+      <input type="image" src="images/paypal/donate_today.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
       <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-      </form>
+      </form> -->
       
-      <!-- <a class="pink button banner" href="https://npo.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=4257">Donate Today</a> -->
+      <a class="pink button banner" href="https://npo.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=4257">Donate Today</a>
     </div>
   </div>
 </div>
