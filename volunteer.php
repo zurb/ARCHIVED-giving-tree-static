e7a1dd7bfa272e446d810ba70bf162b74e4fdfea
Mark Hayes <mark@zurb.com>
2012-08-03 05:03:54 -0700

    replaced tabs with spaces

--- volunteer.php
+++ volunteer.php
@@ -626,18 +626,18 @@ function registerSelect(){
     $('#registerlink').hide();
     var taskId = $(this).val();
     var desc = $(this).find('option:selected').text();
-	if(taskId!==''){
+  if(taskId!==''){
       $('#registerlink').show();
       $('#warehouseform').append('<input type="hidden" name="TaskId" value="'+taskId+'">');
       $('#daytask').text(desc);
-	}
+  }
   });
 }
 <!--Image popup-->
 function imagePopup(){
-	$('.imgthumb').click(function(){
-		$('#largeimage').attr('src',$(this).attr('src'));
-	});
+  $('.imgthumb').click(function(){
+    $('#largeimage').attr('src',$(this).attr('src'));
+  });
 }
 <!--FOR THE READ MORE TOGGLE-->
 function readMoreToggle(){
