8933ba6a4beac905d02a183eeafa47fcc028ffe6
zurbchris <chris@zurb.com>
2012-08-02 19:53:49 -0700

    committing salesforce intro pages

---
+++ host-registration.php
@@ -0,0 +1,21 @@
+<?
+  $page_title = "Donate";
+  $page_template = "donate";  
+?>
+<?php include("includes/_salesforce-header.php"); ?>
+
+<div class="row">
+  <div class="eight columns">
+    <h2>Host Registration Update</h2>
+    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur.</p>
+
+    <form>
+      <label>Address</label>
+      <input></input>
+    </form>
+
+    <a href="#" class="button">Continue</a>
+  </div>
+</div>
+
+<?php include("includes/_salesforce-footer.php");  ?>
