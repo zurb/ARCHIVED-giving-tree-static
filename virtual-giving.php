a045164faca247dfb59f3757b896df9e5ac43780
Jackie Ngo <jackie@zurb.com>
2012-08-03 05:05:48 -0700

    virtual giving tree page

--- virtual-giving.php
+++ virtual-giving.php
@@ -4,11 +4,36 @@
 ?>
 <?php include("includes/_virtual-header.php"); ?>
 
+<div class="container header">
+	<div class="row">
+	<h1 class="purple">Back to School Drive</h1>
+	</div>
+</div>
+
 <div class="row">
+	<div class="three columns">
+    <div class="panel">
+      <h5><a href="virtual-about-fgt.php">About Family Giving Tree</a></h5>
+      <p>Donate to support the drive.</p>
+      <hr>
+      <h5>Live Stats</h5>
+      <ol>
+        <li>Provide Full Backpacks:<br>
+	        <span>$20100.00</span>
+        </li>
+        <li>Provide Supplies <br>
+	        <span>$495.00</span>
+        </li>
+        <li>Support the Drive<br>
+	        <span>$3154.41</span>
+        </li>
+      </ol>
+    </div>
+  </div>
   <div class="nine columns">
     <div class="row">
       <div class="two columns">
-        <img src="http://placehold.it/200x200">
+        <img src="vgt-images/backpacks.png">
       </div>
       <div class="six columns">
         <h5>Backpack</h5>
@@ -28,7 +53,7 @@
 
     <div class="row">
       <div class="two columns">
-        <img src="http://placehold.it/200x200">
+        <img src="vgt-images/supplies.png">
       </div>
       <div class="six columns">
         <h5>Supplies</h5>
@@ -48,7 +73,7 @@
 
     <div class="row">
       <div class="two columns">
-        <img src="http://placehold.it/200x200">
+        <img src="vgt-images/money.png">
       </div>
       <div class="six columns">
         <h5>Money</h5>
@@ -70,25 +95,12 @@
       </div>
     </div>
     <hr>
-    <a href="virtual-giving-details.php" class="button right">Continue</a>
+    
+    <a class="medium banner button purple" href="virtual-checkout.php">Continue</a>
+    
   </div>
 
-  <div class="three columns">
-    <div class="panel">
-      <h5><a href="virtual-about-fgt.php">About Family Giving Tree</a></h5>
-      <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod.</p>
-      <hr>
-      <h5>Goal Status</h5>
-      <p>You are <strong>50%</strong> of the way through.</p>
-      <h5>Top Teams</h5>
-      <ol>
-        <li>Team 1</li>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ol>
-      <a href="#">Read More &raquo;</a>
-    </div>
-  </div>
+  
 
 </div>
 
