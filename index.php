ffa0b2cd4ba2c1fab6f36ad31f33a6ddee4bae83
zurbchris <chris@zurb.com>
2012-08-03 02:09:38 -0700

    update banner button style

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 ?>
 <?php include("includes/_header.php"); ?>
 
-<div class="row featured hide-for-small">
+<div class="row featured">
     <div class="twelve columns">
         <div class="orbit-wrapper" id="featured">
             <div>
                 <div class="content">
                     <h1>You Can Help Our Millionth Child</h1>
                     <p class="lead">This year, we’ll reach our goal of helping one million children locally. You could be the person who fulfils the unique holiday wish of our millionth child.</p>
-                    <a href="who-we-help.php" class="button banner large purple">Donate Today &raquo;</a>
+                    <a href="who-we-help.php" class="button banner purple">Donate Today &raquo;</a>
                 </div>
                 <img src="http://placehold.it/2000x800">
             </div>
@@ -19,7 +19,7 @@
                 <div class="content">
                     <h1>Plant a Seed </h1>
                     <p>Putting a smile on a kid’s face doesn’t just happen. It takes the kindness of those who become part of our community of Elves. People plant seeds by donating their time, a backpack or even a few dollars to fulfill the needs of children in low-income communities.</p>
-                    <a href="donate.php" class="button banner large purple">Donate Today &raquo;</a>
+                    <a href="donate.php" class="button banner purple">Donate Today &raquo;</a>
                 </div>
                 <img src="http://placehold.it/2000x800">
             </div>
@@ -27,7 +27,7 @@
                 <div class="content">
                     <h1>Lead a Holiday Wish Drive</h1>
                     <p>As the holidays get closer, your company or organization can help lead a drive by displaying wish cards for fellow workers, customers, members or friends. </p>
-                    <a href="lead-a-drive.php" class="button banner large purple">Lead a Drive &raquo;</a>
+                    <a href="lead-a-drive.php" class="button banner purple">Lead a Drive &raquo;</a>
                 </div>
                 <img src="http://placehold.it/2000x800">
             </div>
@@ -35,7 +35,7 @@
                 <div class="content">
                     <h1>Volunteer for Your Community</h1>
                     <p>Our roots in the communities we serve go deep thanks to our volunteers. Without them, we wouldn’t be able to put smiles on the faces of the kids we help. </p>
-                    <a href="volunteer.php" class="button banner large purple">Volunteer &raquo;</a>
+                    <a href="volunteer.php" class="button banner purple">Volunteer &raquo;</a>
                 </div>
                 <img src="http://placehold.it/2000x800">
             </div>
