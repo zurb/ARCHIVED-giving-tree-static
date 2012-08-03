d200239bca15213fa5d46fe8aeaa8e70e65daebd
zurbchris <chris@zurb.com>
2012-08-03 06:38:10 -0700

    fixed bug

--- who-we-help.php
+++ who-we-help.php
@@ -29,7 +29,7 @@
         <li><a href="#impact">Making an Impact</a></li>
         <li><a href="#terms">Terms &amp; Cond.</a></li>
         <li><a href="#collect">Collecting Wishes</a></li>
-        <li><a href="#apply">Apply</a></li>
+        <!-- <li><a href="#apply">Apply</a></li> -->
       </ul>
     </div>
     
@@ -41,7 +41,7 @@
         <div class="row">
           <div class="twelve columns">
             <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=201967506944688961163.0004c6570f45ceb532d3e&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=37.631635,-122.217407&amp;spn=0.870062,1.919861&amp;z=9&amp;output=embed"></iframe><br />
-            <p><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=201967506944688961163.0004c6570f45ceb532d3e&amp;hl=en&amp;ie=UTF8&amp;t=h&amp;ll=37.662081,-122.187195&amp;spn=0.869705,1.919861&amp;z=9&amp;source=embed" style="color:#0000FF;text-align:left">Family Giving Tree Agencies</a> in a larger map</small></p>
+            <p><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=201967506944688961163.0004c6570f45ceb532d3e&amp;hl=en&amp;ie=UTF8&amp;t=h&amp;ll=37.662081,-122.187195&amp;spn=0.869705,1.919861&amp;z=9&amp;source=embed" style="">Family Giving Tree Agencies</a> in a larger map</small></p>
             <ul class="block-grid three-up disc">
               <li>Achieve Academy</li>
               <li>Adelante Dual Language Academy</li>
