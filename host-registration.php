0934f803a1cf8bb780739a7f393680252038826a
zurbchris <chris@zurb.com>
2012-08-02 20:36:29 -0700

    committing host reg

--- host-registration.php
+++ host-registration.php
@@ -6,12 +6,69 @@
 
 <div class="row">
   <div class="eight columns">
+    <h2>Host Registration</h2>
+    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur.</p>
+
+    <ul class="link-list">
+      <li><a href="#">A</a></li>
+      <li><a href="#">B</a></li>
+      <li><a href="#">C</a></li>
+      <li><a href="#">D</a></li>
+      <li><a href="#">E</a></li>
+      <li><a href="#">F</a></li>
+      <li><a href="#">G</a></li>
+      <li><a href="#">H</a></li>
+      <li><a href="#">I</a></li>
+      <li><a href="#">J</a></li>
+      <li><a href="#">K</a></li>
+      <li><a href="#">L</a></li>
+      <li><a href="#">M</a></li>
+      <li><a href="#">N</a></li>
+      <li><a href="#">O</a></li>
+      <li><a href="#">P</a></li>
+      <li><a href="#">Q</a></li>
+      <li><a href="#">R</a></li>
+      <li><a href="#">S</a></li>
+      <li><a href="#">T</a></li>
+      <li><a href="#">U</a></li>
+      <li><a href="#">V</a></li>
+      <li><a href="#">W</a></li>
+      <li><a href="#">X</a></li>
+      <li><a href="#">Y</a></li>
+      <li><a href="#">Z</a></li>
+    </ul>
+
+    <form>
+      <select>
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+        <option>Option 4</option>
+      </select>
+    </form>
+
+    <a href="#" class="button">Continue</a>
+  </div>
+</div>
+
+<hr>
+
+<div class="row">
+  <div class="eight columns">
     <h2>Host Registration Update</h2>
     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur.</p>
 
     <form>
       <label>Address</label>
-      <input></input>
+      <input type="text" text="">
+      <label>City</label>
+      <input type="text" text="">
+      <label>Organization Type</label>
+      <select>
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+      </select>
     </form>
 
     <a href="#" class="button">Continue</a>
