34a379936cce58024e4b8bd1ee99c291174090f6
Matt <matt@zurb.com>
2012-08-03 04:59:15 -0700

    Add paypal donate button

--- donate.php
+++ donate.php
@@ -25,7 +25,14 @@
     <div class="donate panel">
       <h3>Give Money</h3>
       <p>Give a one time or monthly monetary donation!</p>
-      <a class="pink button banner" href="https://npo.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=4257">Donate Today</a>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="E8CTCYY6FGSU4">
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      </form>
+      
+      <!-- <a class="pink button banner" href="https://npo.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=4257">Donate Today</a> -->
     </div>
   </div>
 </div>
